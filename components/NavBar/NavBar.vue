e2ebf9e02fda1fd0eb451a12a381075515bf5f0f
Aak-la <486910173@qq.com>
2022-07-06 21:59:49 +0800

    搜索栏

---
+++ components/NavBar/NavBar.vue
@@ -0,0 +1,27 @@
+<template>
+	<view class="nav-bar">
+		<view class="nav-bar-top">
+			<view class="nav-bar-content">
+				<uni-icons type="search" color="#999"></uni-icons>
+				<view class="nav-bar-search=txt">
+					asgdhgsadjgas
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name: "NavBar",
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import'./css/navBar.scss'
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,25 @@
+<template>
+	<view class="home-container">
+		<NavBar></NavBar>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style lang="scss">
+	.contion{
+		@include flex(flex-start)
+	}
+
+</style>
