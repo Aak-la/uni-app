f3b37e561ad8369c83d2e6e253d1fdcb86fbd6f1
Aak-la <486910173@qq.com>
2022-07-07 10:51:21 +0800

    适配微信顶部导航条

--- components/NavBar/NavBar.vue
+++ components/NavBar/NavBar.vue
@@ -1,12 +1,18 @@
 <template>
 	<view class="nav-bar">
 		<view class="nav-bar-top">
-			<view class="nav-bar-content">
+			<view :style="{height:statusHeight+'rpx '}">
+			</view>
+			<view class="nav-bar-content" @click="goSearchPage" :style="{marginRight:marginRight+'rpx'}">
 				<uni-icons type="search" color="#999"></uni-icons>
-				<view class="nav-bar-search=txt">
-					asgdhgsadjgas
+				<view class="nav-bar-search-text">
+					搜索关键字
 				</view>
 			</view>
+		</view>
+		<view :style="{height:statusHeight + 80 +'rpx'}"></view>
+		<view class="" v-for="item in 100" :key="item">
+			{{item}}
 		</view>
 	</view>
 </template>
@@ -14,10 +20,31 @@
 <script>
 	export default {
 		name: "NavBar",
+		created() {
+			this.getSystemInfo()
+		},
 		data() {
 			return {
-
+				statusHeight: 20,
+				marginRight: 0
 			};
+		},
+		methods: {
+			getSystemInfo() {
+				const systemInfo = uni.getSystemInfoSync()
+				systemInfo.statusBarHeight &&
+					(this.statusHeight = systemInfo.statusBarHeight * 2)
+				//#ifdef MP-WEIXIN
+				const menubuttonInfo = uni.getMenuButtonBoundingClientRect()
+				this.statusHeight = menubuttonInfo.top * 2.5
+				this.marginRight = menubuttonInfo.width * 2.5
+				//#endif
+			},
+			goSearchPage(){
+				uni.navigateTo({
+					url:'/pages/search/search'
+				})
+			}
 		}
 	}
 </script>
